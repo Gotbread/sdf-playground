b85e5fbd54c64f48c5ebbfb2167c271e6f0c62e5
Gotbread <19180639+Gotbread@users.noreply.github.com>
2021-09-26 19:55:14 +0200

    cube fractal, without centers

--- Fractal/Application.cpp
+++ Fractal/Application.cpp
@@ -21,13 +21,13 @@ bool Application::Init(HINSTANCE hInstance)
 
 	RegisterClass(&wc);
 
-	bool fullscreen = false;
+	bool fullscreen = true;
 	std::string title = "SDF Fractal";
 
 	if (fullscreen)
 	{
-		unsigned width = 800;
-		unsigned height = 600;
+		unsigned width = GetSystemMetrics(SM_CXSCREEN);
+		unsigned height = GetSystemMetrics(SM_CXSCREEN);
 		hWnd = CreateWindow(wc.lpszClassName, title.c_str(), WS_POPUP, 0, 0, width, height, 0, 0, hInstance, this);
 	}
 	else
@@ -122,6 +122,10 @@ LRESULT CALLBACK Application::WndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM
 		break;
 	case WM_KEYDOWN:
 		keystate[static_cast<char>(LOWORD(wParam))] = true;
+		if (LOWORD(wParam) == VK_ESCAPE)
+		{
+			PostQuitMessage(0);
+		}
 		break;
 	case WM_KEYUP:
 		keystate[static_cast<char>(LOWORD(wParam))] = false;
@@ -220,18 +224,13 @@ bool Application::initShaders()
 		static const float grad_eps = 0.001f;
 		static const float3 lighting_dir = normalize(float3(-3.f, -1.f, 2.f));
 
-		float sdSphere(float3 p, float r)
-		{
-			return length(p) - r;
-		}
-
 		float sdBox(float3 p, float3 size)
 		{
 			float3 q = abs(p) - size;
 			return length(max(q, 0.f)) + min(max(q.x, max(q.y, q.z)), 0.f);
 		}
 
-		float fractal1(float3 p, uint depth)
+		float fractal(float3 p, uint depth)
 		{
 			float d = 1e20;
 			float3 abspos = abs(p);
@@ -250,51 +249,37 @@ bool Application::initShaders()
 					abspos.xy = abspos.yx;
 				}
 
-				abspos.x -= 4.f / 3.f;
-				abspos *= 3.f;
-				scale *= 3.f;
-			}
-			return d;
-		}
-
-		float fractal2(float3 p, uint depth)
-		{
-			float d = 1e20;
-			float3 abspos = abs(p);
-			float scale = 1.f;
-			for (uint iter = 0; iter < depth; ++iter)
-			{
-				float new_d = sdBox(abspos, float3(1.f, 1.f, 1.f)) / scale;
-				d = min(d, new_d);
-
-				if (abspos.z > abspos.y)
-				{
-					abspos.yz = abspos.zy;
-				}
-				if (abspos.y > abspos.x)
-				{
-					abspos.xy = abspos.yx;
-				}
-
+				bool center = true;
 				if (abspos.y > 1.f / 3.f)
 				{
 					abspos.y -= 2.f / 3.f;
+					center = false;
 				}
 				if (abspos.z > 1.f / 3.f)
 				{
 					abspos.z -= 2.f / 3.f;
+					center = false;
 				}
 
-				abspos.x -= 10.f / 9.f;
-				abspos *= 9.f;
-				scale *= 9.f;
+				/*if (center)
+				{
+					abspos.x -= 4.f / 3.f;
+					abspos *= 3.f;
+					scale *= 3.f;
+				}
+				else*/
+				{
+					abspos.x -= 10.f / 9.f;
+					abspos *= 9.f;
+					scale *= 9.f;
+				}
 			}
 			return d;
 		}
 
 		float map(float3 p)
 		{
-			return fractal2(p, 3);
+			return fractal(p, 3);
 		}
 
 		float3 grad(float3 p, float baseline)
